e50a91fc914f0376993ac8000d3162f0250e0a10
taciossbr <tacioss.br@gmail.com>
2018-06-29 16:52:26 -0300

    [RF 01] Importação de dados Junções Agências

---
+++ embratel/ijapp/models.py
@@ -0,0 +1,38 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Juncao(models.Model):
+    vsatname = models.CharField(max_length=8)
+    nmd = models.CharField(max_length=200)
+    ender = models.CharField(max_length=200)
+    municipio = models.CharField(max_length=200)
+    UF = models.CharField(max_length=200)
+    junserviço = models.CharField(max_length=200)
+    compart = models.CharField(max_length=200)
+    desc = models.CharField(max_length=200)
+    nomeserv = models.CharField(max_length=200)
+    fone = models.CharField(max_length=200)
+    primeid = models.CharField(max_length=200)
+    category = models.CharField(max_length=200)
+    serial = models.CharField(max_length=200)
+    softprof = models.CharField(max_length=200)
+    devicetype = models.CharField(max_length=200)
+    ipmgmt = models.CharField(max_length=200)
+    iplan1 = models.CharField(max_length=200)
+    mask = models.CharField(max_length=200)
+    servplan = models.CharField(max_length=200)
+    AIS = models.CharField(max_length=200)
+    OutrouteGroup = models.CharField(max_length=200)
+    FAP = models.CharField(max_length=200)
+    IPGW = models.CharField(max_length=200)
+    Ipipgw = models.CharField(max_length=200)
+    IQoS = models.CharField(max_length=200)
+    IQoSnum = models.CharField(max_length=200)
+    Enable = models.CharField(max_length=200)
+    vadb = models.CharField(max_length=200)
+    vadb1 = models.CharField(max_length=200)
+    vadb2 = models.CharField(max_length=200)
+    Enable = models.CharField(max_length=200)
+    DDR = models.CharField(max_length=200)
---
+++ embratel/ijapp/views.py
@@ -0,0 +1,68 @@
+import csv
+from django.http import HttpResponse
+from django.shortcuts import render
+from django.contrib.admin.views.decorators import staff_member_required
+from .models import Juncao
+from .forms import FileForm
+
+
+@staff_member_required
+def get_file(request):
+    if request.method == 'POST':
+        form = FileForm(request.POST, request.FILES)
+        if form.is_valid():
+            file = request.FILES['file']
+            if file is None:
+                return HttpResponse('invalid')
+            Juncao.objects.all().delete()
+            linhas = []
+            for line in file:
+                linhas.append(line.decode('utf-8'))
+                # input()
+            for linha in linhas[1:]:
+                l = [x.replace('"', '') if isinstance(x, str) else x
+                     for x in linha.split(',')]
+                j = Juncao()
+
+                j.vsatname = l[0]
+                j.nmd = l[1]
+                j.ender = l[2]
+                j.municipio = l[3]
+                j.UF = l[4]
+                j.junserviço = l[5]
+                j.compart = l[6]
+                j.desc = l[7]
+                j.nomeserv = l[8]
+                j.fone = l[9]
+                j.primeid = l[10]
+                j.category = l[11]
+                j.serial = l[12]
+                j.softprof = l[13]
+                j.devicetype = l[14]
+                j.ipmgmt = l[15]
+                j.iplan1 = l[16]
+                j.mask = l[17]
+                j.servplan = l[18]
+                j.AIS = l[19]
+                j.OutrouteGroup = l[20]
+                j.FAP = l[21]
+                j.IPGW = l[22]
+                j.Ipipgw = l[23]
+                j.IQoS = l[24]
+                j.IQoSnum = l[25]
+                j.Enable = l[26]
+                j.vadb = l[27]
+                j.vadb1 = l[28]
+                j.vadb2 = l[29]
+                j.Enable = l[30]
+                j.DDR = l[31]
+
+                j.save()
+                print(j)
+                print(l)
+                input()
+            return HttpResponse('/thanks/')
+    else:
+        form = FileForm()
+
+    return render(request, 'file.html', {'form': form})

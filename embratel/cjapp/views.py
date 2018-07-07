e57c9c1783b850418f7549be719b880aff36da79
tadaos <tadascurso@gmail.com>
2018-07-07 13:52:10 -0300

    T

--- embratel/cjapp/views.py
+++ embratel/cjapp/views.py
@@ -1,6 +1,7 @@
 import json
 from django.http import HttpResponse
 from ijapp.models import Juncao,BDN
+from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
 
 
@@ -8,8 +9,9 @@ from django.views.decorators.csrf import csrf_exempt
 
 
 def index (request):
-    template = loader.get_template('cjapp/templates/index.html')
-    return HttpResponse(template.render(context, request))
+    return render(request, 'index.html')
+    #template = loader.get_template('index.html')
+    #return HttpResponse(template.render())
 
 
 
--- embratel/embratel/settings.py
+++ embratel/embratel/settings.py
@@ -125,3 +125,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_DIRS = 'static'
+
+STATICFILES_DIRS = [
+    STATIC_DIRS
+]

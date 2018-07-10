26b5f46b2b46831f9ce5166e707f094fd4fb30b3
tadaos <tadascurso@gmail.com>
2018-07-09 23:25:33 -0300

    mudanças no front-end

--- embratel/static/controllers/juncao.js
+++ embratel/static/controllers/juncao.js
@@ -1,10 +1,51 @@
 var Juncao = {
 	
+	currentJuncao: {},
+
+	arrJuncao: [],
+
+	arrIdJuncao: [],
+
+	onrender: function(){
+
+		Juncao.arrJuncao = [];
+		
+		Juncao.arrIdJuncao = [];
+
+	},
+
+	/**
+	 * getJuncao realiza uma requisição para a api que retorna os dados da junção
+	 * @param {String} id String contendo o id da junção
+	 * @return {Functino} retorna a requisição
+	 */
 	getJuncao: function(id){
+
+		id = id || $('.menu-busca').val();
 
 		return $.ajax({
 			url: '/api/getjuncao/' + id
-		})
+		});
+
+	},
+
+	/**
+	 * showDados Função que exibe os dados da junção na tela
+	 * @param {Number} juncao id da junção que será exibida
+	 */
+	showDados: function(juncao){
+
+		var html = $('<div data-juncao="' + juncao.junserviço + '" class="juncao">\
+			\
+		</div>');
+
+		Object.keys(juncao).forEach(function(key){
+
+			html.append('<div data-key="' + key + '"><div class="head">' + key + '</div><div class="content">' + juncao[key] +'&nbsp; </div></div>');
+
+		});
+
+		$('.show-juncoes').append(html);;
 
 	}
 	
--- embratel/static/js/default.js
+++ embratel/static/js/default.js
@@ -1,11 +1,46 @@
 var Default = {
-	
-	getJuncao: function(id){
-
-		return $.ajax({
-			url: '/api/getjuncao/' + id
-		})
-
-	}
 
 }
+
+$(function(){
+
+    Page.get('juncao');
+
+    $('.menu-busca').on('keydown', function(e){
+
+        if(e.keyCode===13){
+
+            var srcVal = $('.menu-busca').val();
+
+            var page = $('body').attr('page');
+
+            if(page === 'juncao'){
+
+                Juncao.getJuncao(srcVal).then(function(res){
+
+                    $('.menu-busca').val('');
+
+                    Juncao.currentJuncao = JSON.parse(res);
+
+                    if(typeof Juncao.currentJuncao.error === 'undefined'){
+
+                        Juncao.arrIdJuncao.push(Juncao.currentJuncao.junserviço);
+
+                        Juncao.arrJuncao.push(JSON.parse(res));
+
+                        Juncao.showDados(JSON.parse(res));
+
+                    } else{
+                        alert(Juncao.currentJuncao.error);
+                    }
+
+                });
+
+            } else{
+
+            }
+
+        }
+    });
+
+});

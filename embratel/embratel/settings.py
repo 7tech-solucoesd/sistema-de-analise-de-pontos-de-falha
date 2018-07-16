5dbfff3c2783d7ac379a46fb38f73fe4a5ebdeb6
adenilson <adenilson.elias2@gmail.com>
2018-07-16 01:25:03 -0300

    nao tira o debug pq senao da ruim

--- embratel/embratel/settings.py
+++ embratel/embratel/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '!-ojm32^$06ub^dz5pr9y=)*&k49&^5%=b0u(8pfe4)^9-ou-1'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [get_ip()]
 
